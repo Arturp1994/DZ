0d9feb6251ad328247af0739c49e766b41173056
Arthur <arturiopr1994@gmail.com>
2022-06-20 11:36:50 +0300

    Dz4 did (all)

--- src/p2-homeworks/h4/common/c3-SuperCheckbox/SuperCheckbox.tsx
+++ src/p2-homeworks/h4/common/c3-SuperCheckbox/SuperCheckbox.tsx
@@ -21,7 +21,8 @@ const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
 ) => {
 
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        onChangeChecked(e.currentTarget.checked)
+        onChangeChecked && onChangeChecked(e.currentTarget.checked);
+        onChange && onChange(e)
     }
 
     const finalInputClassName = `${s.checkbox} ${className ? className : ''}`
