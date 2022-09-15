4d3673ad604245f80ad74dce2aa32ae9cc91c1d2
Arthur <arturiopr1994@gmail.com>
2022-09-15 14:09:24 +0300

    Dz11

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -1,10 +1,20 @@
 import React, {useState} from 'react'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
+import css from './HW11.module.css'
 
 function HW11() {
-    const [value1, setValue1] = useState(0)
-    const [value2, setValue2] = useState(100)
+    const [value1, setValue1] = useState<number>(0)
+    const [value2, setValue2] = useState<number>(100)
+    const min = 0
+    const max = 100
+
+    const onChangeDoubleRangeHandler = (value: [number, number]) => {
+        if (value1 !== value[0])
+            setValue1(value[0])
+        if (value2 !== value[1])
+            setValue2(value[1])
+    }
 
     return (
         <div>
@@ -12,26 +22,34 @@ function HW11() {
             homeworks 11
 
             {/*should work (должно работать)*/}
-            <div>
-                <span>{value1}</span>
-                <SuperRange
-                    // сделать так чтоб value1 изменялось
-                />
-            </div>
+            <div className={css.wrapper}>
+                <div>
+                    <span className={css.minValue}>{value1}</span>
+                    <SuperRange
+                        onChangeRange={setValue1}
+                        value={value1}
+                        min={min}
+                        max={max}
+                    />
+                </div>
 
-            <div>
-                <span>{value1}</span>
-                <SuperDoubleRange
-                    // сделать так чтоб value1 и value2 изменялось
-                />
-                <span>{value2}</span>
-            </div>
+                <div>
+                    <span>{value1}</span>
+                    <SuperDoubleRange
+                        min={min}
+                        max={max}
+                        value={[value1, value2]}
+                        onChangeRange={onChangeDoubleRangeHandler}
+                    />
+                    <span>{value2}</span>
+                </div>
 
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeSuperRange/>*/}
-            {/*<AlternativeSuperDoubleRange/>*/}
-            <hr/>
+                <hr/>
+                {/*для личного творчества, могу проверить*/}
+                {/*<AlternativeSuperRange/>*/}
+                {/*<AlternativeSuperDoubleRange/>*/}
+                <hr/>
+            </div>
         </div>
     )
 }
